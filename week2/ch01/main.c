b15e2fe5efc9f05b6443c56152bd954d561072d1
huihui11235 <91480525+huihui11235@users.noreply.github.com>
2021-10-10 17:23:20 +0800

    树的节点声明

--- week2/ch01/main.c
+++ week2/ch01/main.c
@@ -15,3 +15,28 @@ struct BinaryTreeNode
     Tree Left;
     Tree Right;
 };
+//AVL树节点声明
+#ifndef AVLTREE_H
+struct AvlNode;
+typedef struct AvlNode *Position;
+typedef struct AvlNode *AvlTree;
+typedef int ElementType;
+
+AvlTree MakeEmpty(AvlTree T);
+Position Find(int X, AvlTree T);
+Position FindMin(AvlTree T);
+Position FindMax(AvlTree T);
+AvlTree Insert(int X, AvlTree T);
+AvlTree Delete(int X, AvlTree T);
+int Retrieve(Position P);
+void PostOrderTraversal(AvlTree T);
+void InOrderTraversal(AvlTree T);
+void PreOrderTraversal(AvlTree T);
+
+#endif // AVLTREE_H
+struct AvlNode {
+	int Element;
+	AvlTree Left;
+	AvlTree Right;
+	int Height;
+};
