67c7e6b9ea9d595dc1529213999dc7c952b84008
huihui11235 <91480525+huihui11235@users.noreply.github.com>
2021-10-12 20:39:55 +0800

    Update main.c

--- week2/ch03/main.c
+++ week2/ch03/main.c
@@ -1,23 +1,3 @@
-#ifndef AVLTREE_H
-struct AvlNode;
-typedef struct AvlNode *Position;
-typedef struct AvlNode *AvlTree;
-typedef int ElementType;
-
-AvlTree MakeEmpty(AvlTree T);
-Position Find(int X, AvlTree T);
-Position FindMin(AvlTree T);
-Position FindMax(AvlTree T);
-AvlTree Insert(int X, AvlTree T);
-AvlTree Delete(int X, AvlTree T);
-int Retrieve(Position P);
-void PostOrderTraversal(AvlTree T);
-void InOrderTraversal(AvlTree T);
-void PreOrderTraversal(AvlTree T);
-
-#endif // AvlTree.h//
-//AVL树的声明//
-
 #include "AvlTree.h"
 #include <stdio.h>
 #include <stdlib.h>
