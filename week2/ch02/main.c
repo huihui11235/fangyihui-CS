ed87276fe9c20919935df2940f91aff5a9008146
huihui11235 <91480525+huihui11235@users.noreply.github.com>
2021-10-12 20:40:29 +0800

    Update main.c

--- week2/ch02/main.c
+++ week2/ch02/main.c
@@ -1,18 +1,3 @@
-ifndef TREE_H
-struct TreeNode;
-typedef struct TreeNode *Position;
-typedef struct TreeNode *SearchTree;
-SearchTree MakeEmpty(SearchTree T);
-Position Find(int x,SearchTree T);
-Position FindMin(SearchTree T);
-Position FindMax(SearchTree T);
-SearchTree Insert(int x,SearchTree T);
-SearchTree Del(int x,SearchTree T);
-int Retrieve(Position p);
-void InorderTraversal(SearchTree T);
-void PreorderTraversal(SearchTree T);
-#endif // TREE_H
-
 #include "tree.h"
 #include <stdio.h>
 #include <malloc.h>
