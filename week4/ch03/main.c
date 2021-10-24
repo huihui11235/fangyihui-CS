d0b1feffe285453d2635aad92b12818061599c40
huihui11235 <91480525+huihui11235@users.noreply.github.com>
2021-10-24 19:21:33 +0800

    Update main.c

--- week4/ch03/main.c
+++ week4/ch03/main.c
@@ -1,1 +1,68 @@
-cacascasc
+#include <stdio.h>
+#define MAX_SIZE 10
+
+void merging(int* l, int l_size, int* r, int r_size)
+{
+	int i, j, k, temp[MAX_SIZE];
+	i = j = k = 0;
+	while (i < l_size && j < r_size)
+	{
+		if (l[i] < r[j])
+		{
+			temp[k++] = l[i++];
+		}
+		else
+		{
+			temp[k++] = r[j++];
+		}
+	}
+	while (i < l_size)
+	{
+		temp[k++] = l[i++];
+	}
+	while (j < r_size)
+	{
+		temp[k++] = r[j++];
+	}
+
+	for (i = 0; i < l_size + r_size; i++)
+	{
+		l[i] = temp[i];
+	}
+}
+
+
+void mergesort(int a[], int n)
+{
+	int* l = a;
+	int* r = a + n / 2;
+	int l_size = n / 2;
+	int r_size = n - l_size;
+
+	if (n > 1)	
+	{
+		mergesort(l, l_size);	
+		mergesort(r, r_size);	
+		merging(l, l_size, r, r_size);	
+	}
+}
+
+int main(void)
+{
+	int i;
+	int a[10] = { 1,5,2,6,9,7,3,4,0,8 };
+	printf("排序前：");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", a[i]);
+	}
+	mergesort(a, 10);
+	//printf("\n\n共交换数据%d次\n\n", c);
+	printf("排序后：");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("\n\n\n");
+	return 0;
+}
