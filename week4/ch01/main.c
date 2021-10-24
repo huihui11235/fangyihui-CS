3e3bd0ae81fd7b6e45576447d86b41706c203004
huihui11235 <91480525+huihui11235@users.noreply.github.com>
2021-10-24 17:50:35 +0800

    Update main.c

--- week4/ch01/main.c
+++ week4/ch01/main.c
@@ -1,21 +1,22 @@
-#include"SmallHeap.h"
+#include"Heap.h"
+#include<stdio.h>
 //入堆
-void putHeap(int d)//Heap[0]是堆顶
+void putHeap(int d)
 {
-    int now, father, temp;//now是当前指向的元素，father是其父节点
-    Heap[Heap_size] = d;//将新元素入堆
+    int now, father, temp;
+    Heap[Heap_size] = d;
     Heap_size++;
-    now = Heap_size-1;//now指向新元素
+    now = Heap_size-1;
     while (now >= 1){
-        father = now >> 1;//father指向其父节点
-        if (Heap[father] < Heap[now]){//若符合小根堆就直接结束循环
+        father = now >> 1;
+        if (Heap[father] < Heap[now]){
             break;
         }
         else{
-            temp = Heap[now];//若不符合小根堆，就交换父子节点的值
+            temp = Heap[now];
             Heap[now] = Heap[father];
             Heap[father] = temp;
-            now = father;//并将now指向父节点进行下一次判断
+            now = father;
         }
     }
 }
@@ -23,20 +24,53 @@ void putHeap(int d)//Heap[0]是堆顶
 //出堆
 int getHeap()
 {
-    int now = 0, son, res = Heap[0], temp;//now是当前指向的元素，son是其子节点
+    int now = 0, son, res = Heap[0], temp;
     Heap[0] = Heap[Heap_size-1];
     Heap_size--;
     //重新整理成小根堆
-    while ((now * 2 + 1) < Heap_size){//当存在左子树时
+    while ((now * 2 + 1) < Heap_size){
         son = now * 2 + 1;
-        if ((now * 2 + 2) < Heap_size && Heap[now * 2 + 1] > Heap[now * 2 + 2]){//当存在右子树且右子树较小时
+        if ((now * 2 + 2) < Heap_size && Heap[now * 2 + 1] > Heap[now * 2 + 2]){
             son++;
         }
 
-        temp = Heap[now];//若不符合小根堆，就交换父子节点的值
+        temp = Heap[now];
         Heap[now] = Heap[son];
         Heap[son] = temp;
         now = son;
     }
     return res;
+}
+
+
+int k = 3;
+//初始化堆//
+void KthLargest(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++){
+        putHeap(a[i]);
+    }
+    for (i = 0; i < (n-k); i++){
+        getHeap();
+    }
+}
+
+//插入元素后求其第k大值//
+void add(int x)
+{
+    putHeap(x);
+
+    getHeap();
+
+    printf("第3大的值是%d",getHeap());
+
+}
+
+int main()
+{
+    int a[10] = {0,1,2,3,4,5,6,7,8,9};
+    KthLargest(a,10);
+    add(10);
+    return 0;
 }
